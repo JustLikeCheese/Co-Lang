f1619e96c87ee97aee8762dde8bf9dbad544b918
Cheese <854675824@qq.com>
2024-06-14 13:46:04 +0800

    迁移项目库到 "import.lua" 文件中，优化了 dump 函数，新增了 table.find 函数，实现了 CoCompiler 编译器半成品

--- main.lua
+++ main.lua
@@ -1,11 +1,4 @@
-function table.contains(_table, _value)
-  for key, value in pairs(_table) do
-    if value == _value then
-      return true
-    end
-  end
-  return false
-end
+require "import"
 
 CoSyntax = {}
 CoSyntax.codeSplit = function(code)
@@ -46,6 +39,7 @@ CoSyntax.codeSplit = function(code)
       end
     elseif char == ' ' then -- 如果为空格(普通模式)
       word = string.sub(code, startIndex, index - 1) -- 截取字符串
+      startIndex = 0
     elseif char == '\"' then -- 如果为双引号(普通模式)
       word = string.sub(code, startIndex, index - 1) -- 截取字符串
       stringMode = true -- 更新字符串模式
@@ -75,17 +69,64 @@ CoSyntax.codeType = function(code)
     return 0 -- variable
   end
 end
-
-local function dump(_table)
-  local result = ""
-  for key, value in pairs(_table) do
-    result = result .. key .. ":" .. value .. "\n"
+-- Co Compiler
+CO_MAP={
+  "echo"
+}
+CO_TYPE={
+  TYPE_VARIABLE=0,
+  TYPE_STRING=1,
+  TYPE_NUMBER=2
+}
+CoCompiler = {}
+CoStaticBlock = {}
+CoCompiler.compile=function(codes)
+  local result = {}
+  if #codes>0 then
+    local func=codes[1]
+    if CoSyntax.codeType(func) ~= CO_TYPE.TYPE_VARIABLE then
+      return {"!","SyntaxError: 第一个参数必须为函数。"}
+    elseif table.find(CO_MAP,func)==nil then
+      return {"!","SyntaxError: 尝试调用一个未定义的函数。"}
+    else
+      table.insert(result,table.find(CO_MAP,func))
+    end
+  end
+  table.insert(result,#codes-1)
+  for index = 2, #codes do
+    local code = codes[index]
+    local type = CoSyntax.codeType(code)
+    if type == CO_TYPE.TYPE_VARIABLE then -- 如果表达式为变量类型
+      table.insert(result, 0) -- 插入参数类型
+      table.insert(CoStaticBlock,code) -- 插入参数到静态块
+      table.insert(result,#CoStaticBlock) -- 插入参数索引
+    elseif type == CO_TYPE.TYPE_STRING then -- 如果表达式为字符串类型
+      table.insert(result, 1) -- 插入参数类型
+      table.insert(CoStaticBlock,string.sub(code,2,#code-1)) -- 插入参数到静态块
+      table.insert(result,#CoStaticBlock) -- 插入参数索引
+    elseif type == CO_TYPE.TYPE_NUMBER then -- 如果表达式为数字类型
+     table.insert(result, 2) -- 插入参数类型
+     table.insert(CoStaticBlock,code) -- 插入参数到静态块
+     table.insert(result,#CoStaticBlock) -- 插入参数索引
+     else
+      return {"!","SyntaxError: 未知的变量类型。"} -- 返回错误
+    end
   end
   return result
 end
-
-local a = CoSyntax.codeSplit("aa\"ad\"123456\"hi!\"")
-print(dump(a))
-
+local a = CoSyntax.codeSplit("echo aa")
+local b =CoCompiler.compile(a)
+-- print(dump(b))
+-- print(dump(CoStaticBlock))
+print("编译结果：")
+local func=b[1]
+print("调用的函数:"..func.."("..CO_MAP[func]..")");
+local args=b[2]
+print("参数数量:"..args);
+for i=1,args do
+  local type=b[2+i]
+  local index=b[3+i]
+  print("参数类型:"..type.." 参数索引:"..index.." 参数值:"..CoStaticBlock[index])
+end
 -- print('a'=="a")
 -- print(string.sub("abc",1,2))
