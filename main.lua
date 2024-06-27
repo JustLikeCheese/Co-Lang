4b9b4de134b43ce03272f4921358fabef3f60b20
Cheese <854675824@qq.com>
2024-06-27 21:38:16 +0800

    重写 CoState 的 'load' 函数。

--- main.lua
+++ main.lua
@@ -66,8 +66,8 @@ CoSyntax.codeFormat = function(lines)
     local line = lines[i]
         :gsub("\n", "") -- 删除换行符
         :gsub("\t", "") -- 删除制表符
-        :gsub(" ", "") -- 删除空格
-    if line=="" or line:sub(1, 2) == "//" then
+        :gsub(" ", "")  -- 删除空格
+    if line == "" or line:sub(1, 2) == "//" then
       table.remove(lines, i)
       i = i - 1
     end
@@ -114,66 +114,7 @@ CoState.type = function(name)
   return "null"
 end
 
--- CoFunction table
-CoFunction = {}
-CoFunctionTable = {}
-CoFunction.new = function(name, args, body)
-  local func = {}
-  table.insert(func, name)  -- 插入函数名
-  table.insert(func, #args) -- 插入参数长度
-  -- 插入参数
-  for i = 1, #args do
-    table.insert(func, args[i])
-  end
-  -- 插入函数体
-  for i = 1, #body do
-    table.insert(func, body[i])
-  end
-  table.insert(CoFunctionTable, func)
-end
-
-
 CoState.load = function(lines)
-  local CoLineCounter = 1 -- 行数
-  local CoPreLines = {}   -- 预处理行
-  local CoLines = CoSyntax.codeFormat(lines)   -- 处理行
-  while true do           -- 通过两层循环实现 continue
-    while true do
-      local CoLine = nil
-      local CoPreStatus = false
-      if #CoPreLines > 0 then
-        CoLine = CoPreLines[1] -- 获取预处理行
-        CoPreStatus = true
-        table.remove(CoPreLines, 1)
-      else
-        CoLine = CoLines[CoLineCounter] -- 获取当前行
-      end
-      -- 如果语法处理失败，丢出错误
-      local Statements = CoSyntax.codeSplit(CoLine)
-      local CoFunction = Statements[1]
-      if CoFunction == "!" then
-        error(Statements[2])
-      end
-      -- 执行函数
-      table.remove(Statements, 1)
-      if CoFunction == "invoke" then
-        if #Statements == 0 then
-          error("SyntaxError: 缺少函数名。")
-        end
-        if CoState.type(Statements[1]) == "function" then
-          local func = CoState.get(Statements[1])
-        end
-      end
-      -- 计数器自增
-      if not CoPreStatus then
-        CoLineCounter = CoLineCounter + 1 -- 行数加一
-      end
-      break                               -- 等效于 continue
-    end
-    if CoLineCounter > #CoLines then
-      break
-    end
-  end
 end
 
 -- print(dump(CoSyntax.codeSplit("Hello World!")))
